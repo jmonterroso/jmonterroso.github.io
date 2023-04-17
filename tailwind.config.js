dc08e495340331af38076bc59b8389368d304db1
Jayson Monterroso <1420938+jmonterroso@users.noreply.github.com>
2023-04-17 15:59:48 -0400

    feat: adding tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./_layouts/**/*.html", "./_includes/**/*.html", "./_posts/**/*.md", "./index.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
